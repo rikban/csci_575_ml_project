362a15db9ad6b9abd2f5dfa10b374e80cd35ef5b
Kevin Spevak <kevinspevak@gmail.com>
2022-05-04 23:48:04 -0600

    additional change for benchmarking

--- utils/data_manager.py
+++ utils/data_manager.py
@@ -5,6 +5,9 @@ DATA_COLUMNS = ["Date", "Price", "Open", "High", "Low", "Volume", "Change"]
 
 class Datum():
     def __init__(self, values):
+        # fix missing volume data
+        if values[5] == "-":
+            values[5] = "-1"
         self.date = values[0]
         self.price, self.open, self.high, self.low, self.volume, self.change = values[1:].astype(np.float32)
 
@@ -15,10 +18,14 @@ class Datum():
         return ", ".join([DATA_COLUMNS[i] + ": " + str(values[i]) for i in range(len(values))])
 
 class DataManager():
-    def __init__(self, filename="data.csv", rows=2263):
+    # rows = 2263 to avoid the post 2008 volatility
+    def __init__(self, filename="data.csv", rows=None):
         # filename should be relative to repo base directory
         path = "/".join(__file__.split("/")[:-2] + [filename])
         assert(os.path.exists(path))
         with open(path, "r") as f:
             raw = np.genfromtxt(f, dtype='str', delimiter=',', invalid_raise=False, usecols=np.arange(0, 7))
-            self.data = [Datum(raw[i,:]) for i in range(1, rows+1)]
+            if rows is None:
+                self.data = np.flip(np.array([Datum(raw[i,:]) for i in range(1, raw.shape[0])]))
+            else:
+                self.data = np.flip(np.array([Datum(raw[i,:]) for i in range(1, rows+1)]))
